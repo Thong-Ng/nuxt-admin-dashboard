56546e0be92d36e9f5ab612162446ef3c3402289
Thong Ng <huang.wtn@gmail.com>
2025-10-30 13:36:59 +0800

    Backend Integration

--- frontend-admin/nuxt.config.ts
+++ frontend-admin/nuxt.config.ts
@@ -14,7 +14,7 @@ export default defineNuxtConfig({
     'vue-sonner/nuxt',
   ],
   auth: {
-    baseURL: 'http://nuxt-admin-dashboard-two.vercel.app/api/',
+    baseURL: 'https://nuxt-admin-dashboard-two.vercel.app/api/',
     provider: {
       type: 'local',
       pages: {
